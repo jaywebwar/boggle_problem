ea78863dc242eaf7b184d7a8777f4434f3f23443
jay0828 <jay0828@vt.edu>
2018-10-11 14:48:08 -0400

    Works with OX, debugging for more complex scenarios

--- boggle_problem.py
+++ boggle_problem.py
@@ -1,10 +1,10 @@
 #create our board with a couple of built in solutions
 def createBoard():
 	coord = []
-	for i in range(4):
-		for j in range(4):
-			coord.append("("+str(i)+","+str(j)+")")
-	board = {coord[0]:'A', coord[1]:'B', coord[2]:'C', coord[3]:'D', coord[4]:'E', coord[5]:'M', coord[6]:'G', coord[7]:'I', coord[8]:'I', coord[9]:'O', coord[10]:'A', coord[11]:'L', coord[12]:'C', coord[13]:'X', coord[14]:'O', coord[15]:'Z'}
+	for y in range(4):
+		for x in range(4):
+			coord.append((x, y))
+	board = {coord[0]:'M', coord[1]:'B', coord[2]:'C', coord[3]:'D', coord[4]:'A', coord[5]:'M', coord[6]:'G', coord[7]:'I', coord[8]:'G', coord[9]:'O', coord[10]:'A', coord[11]:'L', coord[12]:'C', coord[13]:'X', coord[14]:'O', coord[15]:'Z'}
 	return board, coord
 
 #print the board to the user
@@ -29,15 +29,17 @@ def isWordLegal(board, coord, word, currentLetterIt = None, letPos = None):
 		currentLetterIt = 0
 		#where is currentLetter
 		letPos = findLetterPos(board, coord, word[currentLetterIt])
+	print(str(word[0])+" positions: "+str(letPos))
 	if len(letPos) == 0:
 		#there are no positions
 		return False
 	for number in range(len(letPos)):
 		#check if nextLetter is adjacent to currentLetter
-		isIt, nextPositions = isNextLetterAdjacent(board, coord, letPos.pop(), word[currentLetterIt+1])
-		if isIt:
+		nextPositions = isNextLetterAdjacent(board, letPos.pop(), word[currentLetterIt+1])
+		if len(nextPositions) != 0:
 			#is nextLetter the last letter?
 			if len(word)-1 == currentLetterIt+1:
+				print(str(word[currentLetterIt+1])+" is the next and last letter.")
 				return True
 			#advance letterIt and check if legal
 			currentLetterIt = currentLetterIt +1
@@ -62,11 +64,60 @@ def findLetterPos(board, coord, letter):
 			positions.append(each)
 	return positions
 
-def isNextLetterAdjacent(board, coord, currentLetterPos, nextLetterVal):
-	#This function needs to return bool for whether or not the next
-	#letter is adjacent and return a list of the the adjacent positions
+def isNextLetterAdjacent(board, currentLetterPos, nextLetterVal):
+	#Return a list of the the adjacent positions
 	#that have the matching letter
-	return None, None
+	adjacentPositions = []
+	print("Position we're getting the adjacent positions for: "+str(currentLetterPos))
+	first, second = currentLetterPos
+	x = int(first)
+	y = int(second)
+
+	#map all 8 positions
+	adjacentPositions.append((x-1, y-1))
+	adjacentPositions.append((x-1, y))
+	adjacentPositions.append((x-1, y+1))
+	adjacentPositions.append((x, y-1))
+	adjacentPositions.append((x, y+1))
+	adjacentPositions.append((x+1, y-1))
+	adjacentPositions.append((x+1, y))
+	adjacentPositions.append((x+1, y+1))
+
+	#remove illegal positions
+	#flags to avoid ValueError
+	negXFlag = False
+	posXFlag = False
+	if x-1 < 0:
+		adjacentPositions.remove((x-1, y-1))
+		adjacentPositions.remove((x-1, y))
+		adjacentPositions.remove((x-1, y+1))
+		negXFlag = True
+	if x+1 > 3:
+		adjacentPositions.remove((x+1, y-1))
+		adjacentPositions.remove((x+1, y))
+		adjacentPositions.remove((x+1, y+1))
+		posXFlag = True
+	if y-1 < 0:
+		adjacentPositions.remove((x, y-1))
+		if not negXFlag:
+			adjacentPositions.remove((x-1, y-1))
+		if not posXFlag:
+			adjacentPositions.remove((x+1, y-1))
+	if y+1 > 3:
+		adjacentPositions.remove((x, y+1))
+		if not negXFlag:
+			adjacentPositions.remove((x-1, y+1))
+		if not posXFlag:
+			adjacentPositions.remove((x+1, y+1))
+	
+	#check legal positions for nextLetterVal
+	print("These are the positions that are adjacent to our current letter: "+str(adjacentPositions))
+	adjacentPositionsWithLetterVal = []
+	for each in adjacentPositions:
+		if board[each] == nextLetterVal:
+			adjacentPositionsWithLetterVal.append(str(each))
+	print("These are the positions that are adjacent with the next letter in them: "+str(adjacentPositionsWithLetterVal))
+	return adjacentPositionsWithLetterVal
 
 if __name__ == "__main__":
 	board, coord = createBoard()
