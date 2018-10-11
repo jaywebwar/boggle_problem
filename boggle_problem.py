c585bf20920d47bfcfba9ad1956cc3a9161a6a59
jay0828 <jay0828@vt.edu>
2018-10-11 11:23:25 -0400

    createBoard proto made, next make  printBoard and make a proper setup

--- boggle_problem.py
+++ boggle_problem.py
@@ -1,6 +1,12 @@
 #create our board with a couple of built in solutions
 def createBoard():
-	return None
+	coord = []
+	for i in range(4):
+		for j in range(4):
+			coord.append("("+str(i)+","+str(j)+")")
+	board = {coord[0]:'A', coord[1]:'B', coord[2]:'C', coord[3]:'D', coord[4]:'E', coord[5]:'F', coord[6]:'G', coord[7]:'H', coord[8]:'I', coord[9]:'J', coord[10]:'K', coord[11]:'L', coord[12]:'M', coord[13]:'N', coord[14]:'O', coord[15]:'P'}
+	print(board)
+	return board
 
 #print the board to the user
 def printBoard(board):
@@ -15,11 +21,11 @@ def isWordLegal(board, word):
 	return None
 
 #print whether the word is legal or not
-def printAnswer(bool isLegal):
+def printAnswer(isLegal):
 	return None
 
 if __name__ == "__main__":
 	board = createBoard()
-	printBoard(board)
-	chosenWord = getWord()
-	printAnswer(isWordLegal(board, chosenWord))
+	#printBoard(board)
+	#chosenWord = getWord()
+	#printAnswer(isWordLegal(board, chosenWord))
