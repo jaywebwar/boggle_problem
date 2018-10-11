22480f49bf19cbdb91269a3e9354e2239064a238
jay0828 <jay0828@vt.edu>
2018-10-11 09:54:12 -0400

    init

---
+++ boggle_problem.py
@@ -0,0 +1,25 @@
+#create our board with a couple of built in solutions
+def createBoard():
+	return None
+
+#print the board to the user
+def printBoard(board):
+	return None
+
+#choose a word
+def getWord():
+	return None
+
+#check the board against our chosen word
+def isWordLegal(board, word):
+	return None
+
+#print whether the word is legal or not
+def printAnswer(bool isLegal):
+	return None
+
+if __name__ == "__main__":
+	board = createBoard()
+	printBoard(board)
+	chosenWord = getWord()
+	printAnswer(isWordLegal(board, chosenWord))
